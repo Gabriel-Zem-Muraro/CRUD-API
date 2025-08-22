ffdc315cabd5761bf88ba6b2446023a58e9f031d
Gabriel Muraro <gabriel.muraro@starya.ai>
2025-08-22 16:39:09 -0300

    feat: enable CORS in main.ts and update signIn method to return detailed user information along with access token

--- src/modulos/auth/auth.service.ts
+++ src/modulos/auth/auth.service.ts
@@ -2,6 +2,19 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConsultaUsuario } from '../users/domain/services/consulta_usuarios.service';
 import { JwtService } from '@nestjs/jwt';
 
+export interface UserTipos {
+  access_token: string;
+  user: {
+    id: number;
+    name: string;
+    idade: number;
+    email: string;
+    isActive: boolean;
+    createdAt: string;
+    updatedAt: string;
+  }
+}
+
 @Injectable()
 export class AuthService {
   constructor(
@@ -9,12 +22,24 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async signIn(email: string, pass: string): Promise<{ access_token: string }> {
+  async signIn(email: string, pass: string): Promise<UserTipos> {
     const user = await this.usersService.consultarEmail(email);
     if (user?.password !== pass) {
       throw new UnauthorizedException();
     }
+    
     const payload = { sub: user.id, userEmail: user.name };
-    return { access_token: await this.jwtService.signAsync(payload) };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+      user: {
+        id: user.id,
+        name: user.name,
+        idade: user.idade,
+        email: user.email,
+        isActive: user.is_active,
+        createdAt: user.created_at.toISOString(),
+        updatedAt: user.updated_at.toISOString(),
+      }
+    };
   }
 }
